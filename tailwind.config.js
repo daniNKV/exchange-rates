a56648fe70ae6acd8fbfebfa46ea735f33c68edb
daniNKV <nikiforovdaniel@gmail.com>
2023-03-01 02:48:58 -0300

    Some style to base selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'display': ['Staaltliches'],
+      'sans': ['Roboto']
+    },
+
     extend: {},
   },
   plugins: [],
