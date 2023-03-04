701416fb4dab40c354e829dbfb93f149477adc14
daniNKV <nikiforovdaniel@gmail.com>
2023-03-04 00:17:10 -0300

    Filling the base coin selector with country codes

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ const DEFAULT_BASE = "ARS" ;
 async function getRates() {
     const response = await fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${config.API_KEY}&base=${DEFAULT_BASE}`);
     const rates = await response.json();
-    
+
     return rates.exchange_rates;
 }
 
@@ -14,5 +14,22 @@ async function getFlags($code){
     return flag;
 }
 
+async function initialize() {
 
+}
+
+function appendCoinOption(coinCode) {
+    const $coinsList = document.getElementById('base-coin');
+    $coinsList.appendChild(createOption(coinCode));
+    
+    return
+}
+
+function createOption(code) {
+    const $coinOptionTemplate = document.getElementById('coin-option').cloneNode(true);
+    const option = $coinOptionTemplate.content.querySelector('option')
+    option.value = code;
+    option.textContent = code;
+    return option
+}
 
