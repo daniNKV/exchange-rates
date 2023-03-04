c3b86b73f441e89c54da891e16b1c8a4f67c4bc5
daniNKV <nikiforovdaniel@gmail.com>
2023-03-04 17:42:20 -0300

    Fetch and show historical rates

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,10 @@ async function getRates(base = config.DEFAULT_BASE) {
 }
 
 async function getHistoricRates(date, base = config.DEFAULT_BASE) {
-    // date format = 2020-08-31
+    // date format = YYYY/MM/DD
     const response = await fetch(`https://exchange-rates.abstractapi.com/v1/historical?api_key=${config.API_KEY}&base=${base}&date=${date}`);
     const rates = await response.json();
-
+    
     return rates.exchange_rates;
 }
 
@@ -19,7 +19,8 @@ async function initialize() {
     // const actualRates = await getRates();
     const actualRates = getMockRates();
     const actualDate = parseDate(new Date());
-    showActualDate(actualDate);
+    setActualDate(actualDate);
+    updateDate(actualDate);
     fillBaseSelector(Object.keys(actualRates));
     populateTable(actualRates);
     watchAmountChanges();
@@ -27,14 +28,28 @@ async function initialize() {
     return
 }
 
-function showActualDate(date) {
-    const $dateInput = document.getElementById('actual-date');
-    const $tableDate = document.getElementById('table-date');
-    $tableDate.textContent = date;
+async function update(){
+    const selectedDate = document.getElementById('date-input').value;
+    const selectedCurrency = document.getElementById('base-coin').value;
+    // const newRates = await getHistoricalRates(selectedDate, selectedCurrency);
+    const newRates = getMockHistoricalRates()
+    updateDate(selectedDate);
+    populateTable(newRates);
+
+}
+
+function setActualDate(date) {
+    const $dateInput = document.getElementById('date-input');
+    
     $dateInput.max = date;
     $dateInput.value = date;
     
     return
+}
+
+function updateDate(date) {
+    const $tableDate = document.getElementById('table-date');
+    $tableDate.textContent = date;
 }
 
 // Convert from YYYY/MM/DD to YYYY/DD/MM
@@ -123,6 +138,42 @@ function watchAmountChanges() {
 
 
 document.addEventListener('load', initialize())
+document.getElementById('submit-button').addEventListener('click', update);
+
+function getMockHistoricalRates() {
+    return {
+        "USD": 1.0684,
+        "JPY": 144.82,
+        "BGN": 1.9558,
+        "CZK": 23.426,
+        "DKK": 7.4431,
+        "GBP": 0.88574,
+        "HUF": 373.58,
+        "PLN": 4.6745,
+        "RON": 4.9174,
+        "SEK": 11.1,
+        "CHF": 0.9997,
+        "ISK": 151.7,
+        "NOK": 11.0365,
+        "TRY": 20.1798,
+        "AUD": 1.5776,
+        "BRL": 5.5535,
+        "CAD": 1.4531,
+        "CNY": 7.3349,
+        "HKD": 8.3866,
+        "IDR": 16273.43,
+        "ILS": 3.8729,
+        "INR": 88.0848,
+        "KRW": 1389.03,
+        "MXN": 19.4883,
+        "MYR": 4.7784,
+        "NZD": 1.7048,
+        "PHP": 58.762,
+        "SGD": 1.4316,
+        "THB": 37.063,
+        "ZAR": 19.3769
+      }
+}
 
 function getMockRates() {
     return {
