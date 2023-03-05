4622b395aaf3ff816073f40e1cbf2ead7656719e
daniNKV <nikiforovdaniel@gmail.com>
2023-03-05 20:56:18 -0300

    Lambda functions

---
+++ functions/get-rates/get-rates.js
@@ -0,0 +1,35 @@
+const process = require('process')
+
+const axios = require('axios')
+const qs = require('qs')
+
+const handler = async function (event) {
+  // apply our function to the queryStringParameters and assign it to a variable
+  const { base } = event.queryStringParameters
+  // Get env var values defined in our Netlify site UI
+
+  // TODO: customize your URL and API keys set in the Netlify Dashboard
+  // this is secret too, your frontend won't see this
+  const URL = `https://exchange-rates.abstractapi.com/v1/live/?api_key=${process.env.ABSTRACT_KEY}&base=${base}`
+
+  console.log('Constructed URL is ...', URL)
+
+  try {
+    const { data } = await axios.get(URL)
+    // refer to axios docs for other methods if you need them
+    // for example if you want to POST data:
+    //    axios.post('/user', { firstName: 'Fred' })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (error) {
+    const { data, headers, status, statusText } = error.response
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    }
+  }
+}
+
+module.exports = { handler }
