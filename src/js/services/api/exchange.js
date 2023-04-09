9bd8982d16cfcb3353b83ddd0299d0fac8440ceb
daniNKV <nikiforovdaniel@gmail.com>
2023-04-08 23:56:26 -0300

    Export only one function

--- src/js/services/api/exchange.js
+++ src/js/services/api/exchange.js
@@ -1,21 +1,24 @@
-export const DEFAULT_BASE = 'ARS';
+const DEFAULT_BASE = 'ARS';
 
-export async function getRates(base = DEFAULT_BASE) {
+async function getActualRates(base) {
     const response = await fetch(`.netlify/functions/get-rates?base=${base}`);
     return await response.json().exchange_rates;
-
 }
 
-export async function getHistoricalRates(date, base = DEFAULT_BASE) {
+async function getHistoricalRates(date, base) {
     // date format = YYYY/MM/DD
     const response = await fetch(`.netlify/functions/get-historical-rates?base=${base}&date=${date}`);
     return await response.json().exchange_rates;
 }
 
-function parseToAlpha2(ALPHA_3) {
-    return `${ALPHA_3.slice(0, 2).toLowerCase()}`;
-}
-
-export function retrieveFlagSource(ALPHA_CODE_3) {
+export function getFlagSource(ALPHA_CODE_3) {
+    const parseToAlpha2 = (ALPHA_3) => ALPHA_3.slice(0, 2).toLowerCase();
     return `https://flagcdn.com/28x21/${parseToAlpha2(ALPHA_CODE_3)}.webp`;
 }
+
+export default async function getRates(base = DEFAULT_BASE, date = undefined) {
+    return date 
+        ? getHistoricalRates(date, base) 
+        : getActualRates(base);
+}
+
