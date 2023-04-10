ef3620d8a6d098e87ceff2b12be8769d75dd6bca
daniNKV <nikiforovdaniel@gmail.com>
2023-04-10 15:53:57 -0300

    Refactor test readability

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,48 +1,51 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="Cypress" />
+
 describe('Homepage', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8888/');
-  })
+    it('Logo', () => {
+        cy.getByData('logo').should('be.visible');
+    });
 
-  it('Logo', () => {
-    cy.getByData('logo').should('be.visible');
-  })
+    it('Currency options', () => {
+        cy.get('#rate-selector').should('be.visible').as('container');
 
-  it('Currency options', () => {
-    const selector = cy.get('#rate-selector');
-    selector.should('be.visible');
+        cy.getByData('currency').as('currencySelector');
+        cy.get('@currencySelector').children().get('p').should('contain', 'Select currency:');
+        cy.get('@currencySelector').get('select').should('be.visible');
+        cy.get('@currencySelector').get('select').children().get('option');
 
-    const currencySelector = cy.getByData('currency');
-    currencySelector.children().get('p').should('contain', 'Select currency:');
-    currencySelector.get('select').should('be.visible');
-    currencySelector.get('select').children().get('option');
-    
-    const dateSelector = cy.getByData('date');
-    dateSelector.children().get('p').should('contain', 'Enter the date:');
-    dateSelector.get('input').should('be.visible');
-    dateSelector.get('input').should('have.attr', 'type', 'date');
+        cy.getByData('date').as('dateSelector');
+        cy.get('@dateselector').children().get('p').should('contain', 'Enter the date:');
+        cy.get('@dateselector').get('input').should('be.visible');
+        cy.get('@dateselector').get('input').should('have.attr', 'type', 'date');
 
-    selector.get('button');
-  })
+        cy.get('@container').get('button');
+    });
 
-  it('Rates table', () => {
-    const table = cy.getByData('rates-table');
-    table.should('be.visible');
-    const head = table.children().get('thead');
-    const body = table.children().get('tbody');
+    it('Rates table', () => {
+        cy.getByData('rates-table').should('be.visible').children()
+            .as('table');
+        cy.get('@table').get('thead')
+            .as('head');
+        cy.get('@table').get('tbody')
+            .as('body');
 
-    head.children().get('tr').children().get('th').should('have.length', 2);
-    head.get('#table-date');
-    head.get('#convertion-amount').should('have.attr', 'type', 'number');
-    head.get('#convertion-amount').should('have.attr', 'placeholder', 'Amount to convert');  
-    
-    body.getByData('values');
-    const value = body.getByData('values').first().children();
-    value.should('have.length', 4);
-    value.get('.code').should('contain', 'ARS');
-    value.get('.flag').should('be.visible');
-    value.get('.flag').children().get('img');
-    value.get('.rate')
-    value.get('.value')
+        cy.get('@table')
+            .get('tr').children()
+            .get('th')
+            .should('have.length', 2);
+        cy.get('@table').get('#table-date');
+        cy.get('@table').get('#convertion-amount').should('have.attr', 'type', 'number');
+        cy.get('@table').get('#convertion-amount').should('have.attr', 'placeholder', 'Amount to convert');
 
-  })
-})
+        cy.get('@body').getByData('values');
+        cy.get('@body').getByData('values').first().children()
+            .as('value');
+        cy.get('@value').should('have.length', 4);
+        cy.get('@value').get('.code').should('contain', 'ARS');
+        cy.get('@value').get('.flag').should('be.visible');
+        cy.get('@value').get('.flag').children().get('img');
+        cy.get('@value').get('.rate');
+        cy.get('@value').get('.value');
+    });
+});
