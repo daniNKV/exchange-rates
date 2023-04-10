41a93d463d27d6a7e20eb42bd5a6e56a039b3993
daniNKV <nikiforovdaniel@gmail.com>
2023-04-10 00:49:38 -0300

    Add error handling

--- src/js/services/storage/exchange.js
+++ src/js/services/storage/exchange.js
@@ -1,10 +1,14 @@
-export function saveRates(rates) {
-    if (!rates.base || !rates.date || !rates.coins) throw new Error('Missing parameters to save rates');
-    localStorage.setItem(`rates-${rates.base}-${rates.date}`, JSON.stringify(rates.coins));
+export function saveRates(base, date, coins) {
+    if (!base || !date || !coins) throw new Error('Missing parameters to save rates');
+    localStorage.setItem(`rates-${base}-${date}`, JSON.stringify(coins));
 }
 
 export function getRates(base = '', date = '') {
-    if(!base || !date) throw new Error('Missing parameters to get rates');
-    return JSON.parse(localStorage.getItem(`rates-${base}-${date}`));
+    if (!base || !date) throw new Error('Missing parameters to get rates');
 
+    const rates = localStorage.getItem(`rates-${base}-${date}`);
+
+    if (rates === null) {
+        throw new Error('Rates not found');
+    }
 }
