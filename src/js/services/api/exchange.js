fb657bec096d48c4c4769511b4dcaf8788814f80
daniNKV <nikiforovdaniel@gmail.com>
2023-04-10 00:50:27 -0300

    Return standarized object

--- src/js/services/api/exchange.js
+++ src/js/services/api/exchange.js
@@ -1,16 +1,20 @@
-const DEFAULT_BASE = 'ARS';
 import { getActual, getHistorical } from './api.js';
 
-export async function getRates(base = DEFAULT_BASE, date = '') {
-    const rates = date 
-        ? await getHistorical(date, base) 
-        : await getActual(base);
-    return rates;
-}
+const DEFAULT_BASE = 'ARS';
 
-export function getFlagSource(ALPHA_CODE_3) {
+function getFlagSource(ALPHA_CODE_3) {
     const parseToAlpha2 = (ALPHA_3) => ALPHA_3.slice(0, 2).toLowerCase();
     return `https://flagcdn.com/28x21/${parseToAlpha2(ALPHA_CODE_3)}.webp`;
 }
 
-
+export default async function fetchRates(base = DEFAULT_BASE, date = '') {
+    const rates = date
+        ? await getHistorical(base, date)
+        : await getActual(base);
+    return {
+        base: rates.base,
+        coins: rates.exchange_rates,
+        date: rates.last_updated,
+        flagSource: getFlagSource,
+    };
+}
