97c09334343eddf7fd99ffb57175acca05e0f4e1
daniNKV <nikiforovdaniel@gmail.com>
2023-04-10 17:54:16 -0300

    Change API provider

--- functions/get-rates/get-rates.js
+++ functions/get-rates/get-rates.js
@@ -1,35 +1,34 @@
-const process = require('process')
+// const process = require('process');
+// const keys = process.env.RATES_KEY;
+const axios = require('axios');
 
-const axios = require('axios')
-const qs = require('qs')
-
-const handler = async function (event) {
-  // apply our function to the queryStringParameters and assign it to a variable
-  const { base } = event.queryStringParameters
-  // Get env var values defined in our Netlify site UI
-
-  // TODO: customize your URL and API keys set in the Netlify Dashboard
-  // this is secret too, your frontend won't see this
-  const URL = `https://exchange-rates.abstractapi.com/v1/live/?api_key=${process.env.ABSTRACT_KEY}&base=${base}`
-
-  console.log('Constructed URL is ...', URL)
-
-  try {
-    const { data } = await axios.get(URL)
-    // refer to axios docs for other methods if you need them
-    // for example if you want to POST data:
-    //    axios.post('/user', { firstName: 'Fred' })
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
+const getUrl = (base = '', date = '') => {
+    if (!date) {
+        return `https://api.exchangerate.host/latest${base ? `?base=${base}` : ''}`;
     }
-  } catch (error) {
-    const { data, headers, status, statusText } = error.response
-    return {
-      statusCode: error.response.status,
-      body: JSON.stringify({ status, statusText, headers, data }),
-    }
-  }
-}
+    return `https://api.exchangerate.host/${date}?base=${base}`;
+};
 
-module.exports = { handler }
+const handler = async (e) => {
+    const { base, date } = e.queryStringParameters;
+    const URL = getUrl(base, date);
+    try {
+        const { data } = await axios.get(URL);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        const {
+            data, headers, status, statusText,
+        } = error.response;
+        return {
+            statusCode: error.response.status,
+            body: JSON.stringify({
+                status, statusText, headers, data,
+            }),
+        };
+    }
+};
+
+module.exports = { handler };
